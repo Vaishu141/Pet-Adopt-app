9c47c96038f60926d9a3beaef6fb9b09851a45ad
Vaishu Krish <your_email@example.com>
2025-10-04 15:36:55 +0530

    first commit

--- src/components/router/Router.jsx
+++ src/components/router/Router.jsx
@@ -1,8 +1,4 @@
-import {
-  createBrowserRouter,
-
-} from "react-router-dom";
-
+import { createBrowserRouter } from "react-router-dom";
 
 import Home from "../../pages/home/Home";
 import Login from "../../pages/login/Login";
@@ -46,8 +42,6 @@ import UpdateDonationCampDashboard from "../../pages/userDashboard/updateDonatio
 import PrivateRoute from "./PrivateRoute";
 import AllPetsByCategory from "../../pages/allPetsByCategory/AllPetsByCategory";
 
-
-
 const router = createBrowserRouter([
   {
     path: "/",
@@ -55,102 +49,164 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Home></Home>,
         // loader: () => fetch('https://serversite-pet-adoption.vercel.app/PetCategory')
       },
       {
-        path: '/petlisting',
-        element: <PetListing></PetListing>
+        path: "/petlisting",
+        element: <PetListing></PetListing>,
       },
       {
-        path: '/catagorized_pets/:cat',
+        path: "/catagorized_pets/:cat",
         element: <AllPetsByCategory></AllPetsByCategory>,
-        loader: ({ params }) => fetch(`https://serversite-pet-adoption.vercel.app/petbycategory/${params.cat}`)
+        loader: ({ params }) =>
+          fetch(
+            `https://serversite-pet-adoption.vercel.app/petbycategory/${params.cat}`
+          ),
       },
       {
-        path: '/adoptionreq',
-        element:<PrivateRoute><AdoptionReqDashboard></AdoptionReqDashboard></PrivateRoute>
+        path: "/adoptionreq",
+        element: (
+          <PrivateRoute>
+            <AdoptionReqDashboard></AdoptionReqDashboard>
+          </PrivateRoute>
+        ),
       },
       {
-        path: '/donationcampaign',
-        element: <DonationCampaign></DonationCampaign>
+        path: "/donationcampaign",
+        element: <DonationCampaign></DonationCampaign>,
       },
       {
-        path: '/donationcampaigndetails/:id',
-        element: <PrivateRoute><DonationCampaignDetails></DonationCampaignDetails></PrivateRoute>,
-        loader: ({ params }) => fetch(`https://serversite-pet-adoption.vercel.app/adddonationcamp/${params.id}`)
+        path: "/donationcampaigndetails/:id",
+        element: (
+          <PrivateRoute>
+            <DonationCampaignDetails></DonationCampaignDetails>
+          </PrivateRoute>
+        ),
+        loader: ({ params }) =>
+          fetch(
+            `https://serversite-pet-adoption.vercel.app/adddonationcamp/${params.id}`
+          ),
       },
       {
         path: "/login",
-        element: <Login></Login>
+        element: <Login></Login>,
       },
       {
         path: "/register",
-        element: <Register></Register>
+        element: <Register></Register>,
       },
       {
-        path: '/userdashboard',
-        element: <PrivateRoute><UserDashboard></UserDashboard></PrivateRoute>
+        path: "/userdashboard",
+        element: (
+          <PrivateRoute>
+            <UserDashboard></UserDashboard>
+          </PrivateRoute>
+        ),
       },
       {
-        path: '/addpet',
-        element: <PrivateRoute><AddPetDashboard></AddPetDashboard></PrivateRoute>
+        path: "/addpet",
+        element: (
+          <PrivateRoute>
+            <AddPetDashboard></AddPetDashboard>
+          </PrivateRoute>
+        ),
       },
       {
-        path: '/adoptpet/:id',
-        element: <PrivateRoute><AdoptPet></AdoptPet></PrivateRoute>,
-        loader: ({ params }) => fetch(`https://serversite-pet-adoption.vercel.app/pets/${params.id}`)
+        path: "/adoptpet/:id",
+        element: (
+          <PrivateRoute>
+            <AdoptPet></AdoptPet>
+          </PrivateRoute>
+        ),
+        loader: ({ params }) =>
+          fetch(`https://serversite-pet-adoption.vercel.app/pets/${params.id}`),
       },
       {
-        path: '/myaddedpets',
-        element: <PrivateRoute><MyAddedPetsDashboard></MyAddedPetsDashboard></PrivateRoute>
+        path: "/myaddedpets",
+        element: (
+          <PrivateRoute>
+            <MyAddedPetsDashboard></MyAddedPetsDashboard>
+          </PrivateRoute>
+        ),
       },
       {
-        path: '/createdonationcamp',
-        element: <PrivateRoute><CreateDonationCampaignDashboard></CreateDonationCampaignDashboard></PrivateRoute>
+        path: "/createdonationcamp",
+        element: (
+          <PrivateRoute>
+            <CreateDonationCampaignDashboard></CreateDonationCampaignDashboard>
+          </PrivateRoute>
+        ),
       },
       {
-        path: '/mydonation',
-        element:<PrivateRoute> <MyDonationDashboard></MyDonationDashboard></PrivateRoute>
+        path: "/mydonation",
+        element: (
+          <PrivateRoute>
+            {" "}
+            <MyDonationDashboard></MyDonationDashboard>
+          </PrivateRoute>
+        ),
       },
       {
-        path: '/updatedonationcamp/:donationCampaignId',
-        element: <PrivateRoute><UpdateDonationCampDashboard></UpdateDonationCampDashboard></PrivateRoute>,
-        loader: ({ params }) => fetch(`https://serversite-pet-adoption.vercel.app/adddonationcamp/${params.donationCampaignId}`)
+        path: "/updatedonationcamp/:donationCampaignId",
+        element: (
+          <PrivateRoute>
+            <UpdateDonationCampDashboard></UpdateDonationCampDashboard>
+          </PrivateRoute>
+        ),
+        loader: ({ params }) =>
+          fetch(
+            `https://serversite-pet-adoption.vercel.app/adddonationcamp/${params.donationCampaignId}`
+          ),
       },
 
-
       {
-        path: '/updatepet/:petId',
-        element: <PrivateRoute><UpdatePetDashboard></UpdatePetDashboard></PrivateRoute>,
-        loader: ({ params }) => fetch(`https://serversite-pet-adoption.vercel.app/pets/${params.petId}`)
-
+        path: "/updatepet/:petId",
+        element: (
+          <PrivateRoute>
+            <UpdatePetDashboard></UpdatePetDashboard>
+          </PrivateRoute>
+        ),
+        loader: ({ params }) =>
+          fetch(
+            `https://serversite-pet-adoption.vercel.app/pets/${params.petId}`
+          ),
       },
       {
-        path: '/mydonationcamp',
-        element: <PrivateRoute><MyDonationCampDashboard></MyDonationCampDashboard></PrivateRoute>
+        path: "/mydonationcamp",
+        element: (
+          <PrivateRoute>
+            <MyDonationCampDashboard></MyDonationCampDashboard>
+          </PrivateRoute>
+        ),
       },
       {
-        path: '/allusers',
-        element: <PrivateRoute><AllUsersDeshboard></AllUsersDeshboard></PrivateRoute>
+        path: "/allusers",
+        element: (
+          <PrivateRoute>
+            <AllUsersDeshboard></AllUsersDeshboard>
+          </PrivateRoute>
+        ),
       },
       {
-        path: '/allpetsadmin',
-        element: <PrivateRoute><AllPetsAdminDashboard></AllPetsAdminDashboard></PrivateRoute>
+        path: "/allpetsadmin",
+        element: (
+          <PrivateRoute>
+            <AllPetsAdminDashboard></AllPetsAdminDashboard>
+          </PrivateRoute>
+        ),
       },
 
       {
-        path: '/alldonationcampadmin',
-        element: <PrivateRoute><AllDonationCampAdminDashboard></AllDonationCampAdminDashboard></PrivateRoute>
+        path: "/alldonationcampadmin",
+        element: (
+          <PrivateRoute>
+            <AllDonationCampAdminDashboard></AllDonationCampAdminDashboard>
+          </PrivateRoute>
+        ),
       },
-
-
-
-
-
-    ]
+    ],
   },
-
 ]);
 export default router;
